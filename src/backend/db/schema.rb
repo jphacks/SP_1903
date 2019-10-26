4e8f7f89199f9abd91334a14af007212f0bac8d9
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2019-10-26 12:40:11 +0900

    Add foreign key of user to postit model

--- src/backend/db/schema.rb
+++ src/backend/db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_26_032431) do
+ActiveRecord::Schema.define(version: 2019_10_26_033548) do
 
   create_table "postits", force: :cascade do |t|
     t.string "test"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_postits_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
