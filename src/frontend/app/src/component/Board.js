63050901e3c77fa7bbf132ea5e0d327627f71c8f
daleksprinter <b1015120@fun.ac.jp>
2019-10-26 11:46:59 +0900

    update board component

--- src/frontend/app/src/component/Board.js
+++ src/frontend/app/src/component/Board.js
@@ -1,10 +1,71 @@
 import React from 'react';
-
+import Postit from './Postit';
 
 class Board extends React.Component {
-  render(){
+　
+　constructor(){
+    super();
+    this.state = {
+        allposts : [
+            {
+                "user" : "user_a",
+                "text" : "text"
+            },
+            {
+                "user" : "user_a",
+                "text" : "text"
+            },
+            {
+                "user" : "user_a",
+                "text" : "text"
+            },
+        ],
+        myposts : [
+            {
+                "text" : "text",
+            },
+            {
+                "text" : "text",
+            },
+            {
+                "text" : "text",
+            }
+        ],
+    }
+  }
+
+  componentDidMount(){
+    const url = "http://localhost:3000";
+    /*
+    fetch(url).then(res => {
+        this.setState({posts : res})
+    })
+    */
+  }
+　　
+  render(props, state){
       return (
-          <div>hoge</div>
+          <div>
+              <div className = "board">
+                {this.state.allposts.map((post) => {
+                    return (
+                        <Postit data = {post} />
+                    )
+                })}
+              </div>
+
+              <div className = "postit_area">
+                  {this.state.myposts.map((post) => {
+                      return (
+                         <div>
+                            <button className = 'send'>send</button>
+                            <Postit data = {post} />
+                         </div>
+                         
+                      )
+                  })}
+              </div>
+          </div>
       )
   }
 }
